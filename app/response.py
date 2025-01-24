6ebd5f7551edf0f437b60b03a0c86fbfa1a2c9cb
Mo-Adel1 <102308283+Mo-Adel1@users.noreply.github.com>
2025-01-24 07:18:14 +0200

    implement one interface for response

--- app/response.py
+++ app/response.py
@@ -1,6 +1,19 @@
-def create_response(status_code, body, content_type="text/html"):
-    status_messages = {200: "OK", 404: "Not Found", 400: "Bad Request", 405: "Method Not Allowed"}
+import json
+
+def response(status_code, message, content_type="application/json"):
+    status_messages = {
+        200: "OK",
+        404: "Not Found",
+        400: "Bad Request",
+        405: "Method Not Allowed",
+        500: "Internal server error",
+    }
     status_message = status_messages.get(status_code, "OK")
+    
+    if isinstance(message, dict):
+        message = json.dumps(message)
+    
     response_line = f"HTTP/1.1 {status_code} {status_message}\r\n"
-    headers = f"Content-Type: {content_type}\r\nContent-Length: {len(body)}\r\n\r\n"
-    return response_line + headers + body
+    headers = f"Content-Type: {content_type}\r\nContent-Length: {len(message)}\r\n\r\n"
+    
+    return response_line + headers + message
