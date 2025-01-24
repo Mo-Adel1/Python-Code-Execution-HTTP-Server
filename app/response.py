44e0549d86f76134ee5accce1c3e1edabc07a072
Mo-Adel1 <102308283+Mo-Adel1@users.noreply.github.com>
2025-01-24 02:44:02 +0200

    finish Level 1: Basic Code Execution

--- app/response.py
+++ app/response.py
@@ -1,6 +1,6 @@
-def create_response(status_code, result_executed_code, content_type="text/html"):
+def create_response(status_code, body, content_type="text/html"):
     status_messages = {200: "OK", 404: "Not Found", 400: "Bad Request", 405: "Method Not Allowed"}
     status_message = status_messages.get(status_code, "OK")
-    response_line = f"HTTP/1.1 {200} {status_message}\r\n"
-    headers = f"Content-Type: {content_type}\r\nContent-Length: {len(result_executed_code)}\r\n\r\n"
-    return response_line + headers + result_executed_code
+    response_line = f"HTTP/1.1 {status_code} {status_message}\r\n"
+    headers = f"Content-Type: {content_type}\r\nContent-Length: {len(body)}\r\n\r\n"
+    return response_line + headers + body
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,27 @@
-import io
 import json
 import sys
+import io
+import traceback
 from response import create_response
+
 def execute_handler(method, path, headers, body):
     if method == "POST":
         try:
             body = json.loads(body)
             code = body.get("code")
+
             if not code:
                 return create_response(400, json.dumps({"error": "Missing 'code' field"}), content_type="application/json")
 
-            stdout = io.StringIO()
-            stderr = io.StringIO()
-            sys.stdout = stdout
-            sys.stderr = stderr
-
-            exec(code, {})
-
-            if stdout:
-                return create_response(200, json.dumps({"stdout": stdout.getvalue()}), content_type="application/json") 
-            if stderr:
-                return json.dumps({"stderr": "error"})
-
-            return json.dumps({"error": "error"})
-        
+            result = execute_user_code(code)
+            if "error" in result:
+                return create_response(400, json.dumps({"error": result["error"]}), content_type="application/json")
+            elif result["stderr"] and result["stdout"]:
+                return create_response(200, json.dumps({"stdout": result.get("stdout", ""), "stderr": result["stderr"]}), content_type="application/json")
+            elif result["stderr"]:
+                return create_response(200, json.dumps({"stderr": result["stderr"]}), content_type="application/json")
+            else:
+                return create_response(200, json.dumps({"stdout": result.get("stdout", "")}), content_type="application/json")
         except json.JSONDecodeError:
             return create_response(400, json.dumps({"error": "Invalid JSON payload"}), content_type="application/json")
 
@@ -32,3 +30,17 @@ def execute_handler(method, path, headers, body):
 
     else:
         return create_response(405, json.dumps({"error": "Method Not Allowed"}), content_type="application/json")
+
+def execute_user_code(code):
+    stdout = io.StringIO()
+    stderr = io.StringIO()
+    sys.stdout = stdout
+    sys.stderr = stderr
+    
+    try:
+        exec(code, {})
+        return {"stdout": stdout.getvalue(), "stderr": ""}
+    except Exception:
+        if stdout.getvalue:
+            return {"stdout": stdout.getvalue(), "stderr": traceback.format_exc()}
+        return {"stderr": traceback.format_exc(), "stdout": ""}
